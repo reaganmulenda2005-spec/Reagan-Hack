ceeda5d5899a2264d0c11b51ba1d83dca7cb0488
reaganmulenda2005-spec <reaganmulenda2005@gmail.com>
2026-01-27 18:34:35 +0200

    Create bug.js

---
+++ utils/bug.js
@@ -0,0 +1,24 @@
+async function bug(message, client, texts, num) {
+    try {
+        const remoteJid = message.key?.remoteJid;
+        await client.sendMessage(remoteJid, {
+            image: { url: `database/${num}.jpg` },
+            caption: `> ${texts}`,
+            contextInfo: {
+                externalAdReply: {
+                    title: "Join Our WhatsApp Channel",
+                    body: "Esprit Digital",
+                    mediaType: 1,
+                    thumbnailUrl: `https://whatsapp.com/channel/0029VbBn6EqLCoX4a38eie0M`,
+                    renderLargerThumbnail: false,
+                    mediaUrl: `${num}.jpg`,
+                    sourceUrl: `${num}.jpg`
+                }
+            }
+        });
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+export default bug;
