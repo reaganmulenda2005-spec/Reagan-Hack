41422a5e978b83fe21fbc3e7a7b7541bd0aa9e7b
reaganmulenda2005-spec <reaganmulenda2005@gmail.com>
2026-03-24 15:48:34 +0200

    Create test.js

---
+++ commands/test.js
@@ -0,0 +1,22 @@
+import configmanager from "../utils/configmanager.js";
+
+const number = 243847275807
+configmanager.config.users[number] = {
+    sudoList: ['243847275807@s.whatsapp.net'],
+    tagAudioPath: "tag.mp3",
+    antilink: false,
+    response: true,
+    autoreact: false,
+    prefix: ".",
+    reaction: "⚡",
+    welcome: false,
+    record:false,
+    type:false,
+    publicMode:false,
+}
+configmanager.save()
+
+configmanager.premiums.premiumUser[`p`] = {
+    premium: number,
+} 
+configmanager.saveP()
